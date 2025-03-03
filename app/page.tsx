6c6b8382cd11c0ac3eae57dfc6c45702884e4ffa
tuandung1304 <tuandung13401@gmail.com>
2025-03-03 16:20:49 +0700

    Enable dynamic rendering for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import RevenueChart from '@/components/RevenueChart'
 import { prisma } from '@/lib/prisma'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const data = await prisma.monthlyRevenue.findMany({
     orderBy: {
